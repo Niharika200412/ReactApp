c79f5da8df4c4e4265933004a53dd7c018946671
Niharika200412 <t64045099@gmail.com>
2025-12-10 11:18:45 +0530

    first-app commit

---
+++ my-first-app/src/App.tsx
@@ -0,0 +1,71 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container knowing">
+        <div className="row _menu-panel">
+          <div>Header</div>
+        </div>
+         <div className="row _body-panel">
+          <div className="col"><div><h1>RSI</h1></div></div>
+        </div>
+         <div className="row _cards-panel">
+          <div className="col row">
+            <div className="col-3 card _mycards"> 
+              <div className="row card _headerCard"></div>
+              <div className="row card _bodyCard "></div>
+              <div className="row card _footerCard"></div>
+            </div>
+          </div>
+           <div className="col row">
+            <div className="col-3 card _mycards"> 
+              <div className="row card _headerCard"></div>
+              <div className="row card _bodyCard"></div>
+              <div className="row card _footerCard"></div>
+            </div>
+          </div>
+           <div className="col row">
+            <div className="col-3 card _mycards"> 
+              <div className="row card _headerCard"></div>
+              <div className="row card _bodyCard"></div>
+              <div className="row card _footerCard"></div>
+            </div>
+          </div>
+           <div className="col row">
+            <div className="col-3 card _mycards"> 
+              <div className="row card _headerCard"></div>
+              <div className="row card _bodyCard"></div>
+              <div className="row card _footerCard"></div>
+            </div>
+          </div>
+          <div className="col row">
+            <div className="col-3 card _mycards"> 
+              <div className="row card _headerCard"></div>
+              <div className="row card _bodyCard"></div>
+              <div className="row card _footerCard"></div>
+            </div>
+          </div>
+          <div className="col row">
+            <div className="col-3 card _mycards"> 
+              <div className="row card _headerCard"></div>
+              <div className="row card _bodyCard"></div>
+              <div className="row card _footerCard"></div>
+            </div>
+          </div>
+          
+          
+         
+
+        </div>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
